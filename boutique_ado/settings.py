c72deae77bf0bd9645e5c79c0f3b5aa503d39680
lisa jones <lisadjones75@gmail.com>
2024-05-21 09:51:45 +0000

    add stripe elements

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lisaloudnes-boutiqueado-v8aegnkyymo.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['8000-lisaloudnes-boutiqueado-v8aegnkyymo.ws-eu111.gitpod.io',
+'8000-lisaloudnes-boutiqueado-v8aegnkyymo.ws-eu114.gitpod.io']
 
 
 # Application definition
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PInsZ1HFYHZGCAW8iMZuwmC2RqAMcEFHc0sQw87pace1QQ7XJjdafRn3qAKoelzX6S0kr38dH31fynbX2vwPVMP00rnMn9pn2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
